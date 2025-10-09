7b2bab8e4417aaabea6e55dd8ba66d053e154339
Gribz <gribzbadi@gmail.com>
2025-10-09 21:35:45 +0300

    moded workouts table abit.....added fillables

--- app/Models/Workout.php
+++ app/Models/Workout.php
@@ -15,7 +15,13 @@ class Workout extends Model
         'reps',
         'date',
     ];
-
+    //added this for flutter frontend sake
+    protected $casts = [
+        'weight' => 'float',
+        'reps' => 'integer',
+        'date' => 'date:Y-m-d',
+    ];
+    //ends here
     public function user()
     {
         return $this->belongsTo(User::class);
